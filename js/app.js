d57b16a79d0c201e185cd30b49fef861bdfcdb29
enkeyz <klarnorbert@gmail.com>
2021-02-18 15:06:03 +0100

    fixed small issues
    Signed-off-by: enkeyz <klarnorbert@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const loading_screen = document.querySelector(".loading-screen");
 const tab_button_container = document.querySelector(".tab-button-container");
 const tab_buttons = document.querySelectorAll(".btn--tab");
@@ -7,14 +9,6 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     loading_screen.classList.add("finished-loading");
   }, 1000);
-});
-
-tab_button_container.addEventListener("click", (event) => {
-  if (!event.target.classList.contains("btn--tab")) return;
-
-  deselectAllTabButton();
-  deselectAllTabs();
-  selectTab(event.target);
 });
 
 const selectTab = (button) => {
@@ -35,3 +29,11 @@ const deselectAllTabs = () => {
     tab_content_item.classList.remove("tab-content__item--active");
   });
 };
+
+tab_button_container.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("btn--tab")) return;
+
+  deselectAllTabButton();
+  deselectAllTabs();
+  selectTab(event.target);
+});
